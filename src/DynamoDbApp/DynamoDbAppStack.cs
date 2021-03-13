2c9d12ee73b866af3b3aa8e9b99c55f0c1d03518
collin-smith <74462915+collin-smith@users.noreply.github.com>
2021-03-13 16:31:48 -0700

    Initial commit

---
+++ src/DynamoDbApp/DynamoDbAppStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace DynamoDbApp
+{
+    public class DynamoDbAppStack : Stack
+    {
+        internal DynamoDbAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ src/DynamoDbApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DynamoDbApp
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new DynamoDbAppStack(app, "DynamoDbAppStack");
+            app.Synth();
+        }
+    }
+}
